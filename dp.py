a44ad7d2c02dd68e9a15f392047f705ec95b6ae5
icebox <464752812@qq.com>
2025-10-17 19:27:23 +0800

    done

--- dp.py
+++ dp.py
@@ -39,4 +39,50 @@ print(test_embedding[:10])
 
 from pymilvus import MilvusClient
 
-milvus_client = MilvusClient("milvus_demo.db")
+milvus_client = MilvusClient(uri="./milvus_demo.db")
+
+collection_name = "my_rag_collection"
+if milvus_client.has_collection(collection_name):
+    print(f"Dropping existing collection: {collection_name}")
+    milvus_client.drop_collection(collection_name)
+
+
+from glob import glob
+
+text_lines = []
+
+for file_path in glob("milvus_docs/en/faq/*.md", recursive=True):
+    with open(file_path, "r") as file:
+        file_text = file.read()
+
+    text_lines += file_text.split("# ")
+
+
+from tqdm import tqdm
+
+data = []
+
+doc_embeddings = embedding_model.encode_documents(text_lines)
+
+for i, line in enumerate(tqdm(text_lines, desc="Creating embeddings")):
+    data.append({"id": i, "vector": doc_embeddings[i], "text": line})
+
+milvus_client.insert(collection_name=collection_name, data=data)
+
+
+question = "How is data stored in milvus?"
+
+search_res = milvus_client.search(
+    collection_name=collection_name,
+    data=embedding_model.encode_queries([question]),  # 将问题转换为嵌入向量
+    limit=3,  # 返回前3个结果
+    search_params={"metric_type": "IP", "params": {}},  # 内积距离
+    output_fields=["text"],  # 返回 text 字段
+)
+
+import json
+
+retrieved_lines_with_distances = [
+    (res["entity"]["text"], res["distance"]) for res in search_res[0]
+]
+print(json.dumps(retrieved_lines_with_distances, indent=4))
